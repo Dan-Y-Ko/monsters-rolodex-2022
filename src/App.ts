ea99fd442516fced6ab6af08d976b25826996793
Dan Ko <danko56666@gmail.com>
2022-10-31 21:07:16 -0400

    understanding generics

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react";
+import { StringLiteral } from "typescript";
 
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
+
+import { getData } from "./utils/data.utils";
+
+type Monster = {
+  id: string;
+  name: String;
+  email: string;
+};
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
@@ -10,9 +19,15 @@ const App = () => {
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((response) => response.json())
+    //   .then((users) => setMonsters(users));
+
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+    };
   }, []);
 
   useEffect(() => {
